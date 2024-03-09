7008c92ad3c2783653da98c3031ca6f22970299b
yassineka <144553726+Yassineka99@users.noreply.github.com>
2024-03-09 16:09:30 +0100

    Add files via upload

---
+++ Location_Voiture/src/main/java/chess/location_voiture/Location_Voiture.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package chess.location_voiture;
+
+/**
+ *
+ * @author Yassine
+ */
+class Personne{
+    String name ;
+    String family ;
+    String cin ;
+    String phone ;
+    String licence ;
+}
+public class Location_Voiture {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
